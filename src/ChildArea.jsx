77347215c3fed94d44b54fae2dc5c9ca4a055bcd
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-08-30 02:15:15 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,29 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがれんだりんぐされた！！");
+
+  const deta = [...Array(2000).keys()];
+  deta.forEach(() => {
+    console.log("...");
+  });
+
+  console.log(deta);
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
